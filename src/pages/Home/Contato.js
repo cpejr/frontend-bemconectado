72c5d69738d2908e971e6c31189ce92eb7538dcf
Arthur <arthurdeolima@gmail.com>
2020-04-28 17:15:43 -0300

    Melhorando Contato e colocando logo

--- src/pages/Home/Contato.js
+++ src/pages/Home/Contato.js
@@ -7,10 +7,13 @@ import { Container, Col, Row } from 'react-bootstrap';
 export default function Contato(){
     return (
         <Container style={{height: "100%"}}>
-            <Row>
+            <Row style={{"margin-top": "75px"}}>
                 <Col>
-                    <h1 style={{"margin-top": "75px"}}>CONTATO</h1>
+                    <h1>CONTATO</h1>
                     <div className='infoLine' />
+                </Col>
+                <Col>
+                    <img src="./logoFinal.png" className="logoFinal" alt="LogoCPE"/>
                 </Col>
             </Row>
             
@@ -18,35 +21,35 @@ export default function Contato(){
                 <div className="centerContainer">
                     <div className="infoContainer">
                         <div className="iconContainer">
-                            <IconContext.Provider value={{ size: '3em' }}>
+                            <IconContext.Provider value={{ size: '2.0em' }}>
                                 <MdLocalPhone />
                             </IconContext.Provider>
                         </div>
-                        <a className="infoData" href="tel:31997791734">(31) 99779-1734</a>
+                        <a className="infoData littleMargin" href="tel:31997791734">(31) 99779-1734</a>
                     </div>
                     <div className="infoContainer">
                         <div className="iconContainer">
-                                <IconContext.Provider value={{ size: '3em' }}>
+                                <IconContext.Provider value={{ size: '2.0em' }}>
                                     <MdEmail />
                                 </IconContext.Provider>
                         </div>
-                        <p className="infoData">bemconectado@cpejr.com.br</p>
+                        <p className="infoData littleMargin">bemconectado@cpejr.com.br</p>
                     </div>
                     <div className="infoContainer">
                         <div className="iconContainer">
-                                <IconContext.Provider value={{ size: '3em' }}>
+                                <IconContext.Provider value={{ size: '2.0em' }}>
                                     <AiOutlineGlobal />
                                 </IconContext.Provider>
                         </div>
-                        <a href="https://cpejr.com.br/site/" className="infoData">www.cpejr.com.br</a>
+                        <a href="https://cpejr.com.br/site/" className="infoData littleMargin">www.cpejr.com.br</a>
                     </div>
                     <div className="infoContainer">
                         <div className="iconContainer">
-                                <IconContext.Provider value={{ size: '3em' }}>
+                                <IconContext.Provider value={{ size: '2.0em' }}>
                                     <MdPlace />
                                 </IconContext.Provider>
                         </div>
-                        <a className="infoData" target="_blank" rel="noopener noreferrer" href="http://maps.google.com/maps?q=CPE - Consultoria e Projetos Elétricos Júnior, Universidade Federal de Minas Gerais - Avenida Presidente Antônio Carlos - Campus Pampulha, Belo Horizonte - MG" >
+                        <a className="infoData littleMargin" target="_blank" rel="noopener noreferrer" href="http://maps.google.com/maps?q=CPE - Consultoria e Projetos Elétricos Júnior, Universidade Federal de Minas Gerais - Avenida Presidente Antônio Carlos - Campus Pampulha, Belo Horizonte - MG" >
                             Av. Presidente Antônio Carlos, 6627, CPDEE, sala 112, UFMG Campus Pampulha, Belo Horizonte – MG
                         </a>
                     </div>
