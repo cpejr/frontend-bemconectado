755c298cb67e3877f0f18c39fc0f4d09d8ac7de1
Arthur <arthurdeolima@gmail.com>
2020-04-28 14:10:50 -0300

    Melhorando Contato

--- src/pages/Home/Contato.js
+++ src/pages/Home/Contato.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MdLocalPhone, MdEmail } from "react-icons/md";
+import { MdLocalPhone, MdEmail, MdPlace } from "react-icons/md";
 import { AiOutlineGlobal } from "react-icons/ai";
 import { IconContext } from "react-icons";
 import { Container, Col, Row } from 'react-bootstrap';
@@ -22,7 +22,7 @@ export default function Contato(){
                                 <MdLocalPhone />
                             </IconContext.Provider>
                         </div>
-                        <a className="infoData" href={`tel:3678-3672`}>3678-3672</a>
+                        <a className="infoData" href="tel:31997791734">(31) 99779-1734</a>
                     </div>
                     <div className="infoContainer">
                         <div className="iconContainer">
@@ -39,6 +39,16 @@ export default function Contato(){
                                 </IconContext.Provider>
                         </div>
                         <a href="https://cpejr.com.br/site/" className="infoData">www.cpejr.com.br</a>
+                    </div>
+                    <div className="infoContainer">
+                        <div className="iconContainer">
+                                <IconContext.Provider value={{ size: '3em' }}>
+                                    <MdPlace />
+                                </IconContext.Provider>
+                        </div>
+                        <a className="infoData" target="_blank" rel="noopener noreferrer" href="http://maps.google.com/maps?q=CPE - Consultoria e Projetos Elétricos Júnior, Universidade Federal de Minas Gerais - Avenida Presidente Antônio Carlos - Campus Pampulha, Belo Horizonte - MG" >
+                            Av. Presidente Antônio Carlos, 6627, CPDEE, sala 112, UFMG Campus Pampulha, Belo Horizonte – MG
+                        </a>
                     </div>
                 </div>
             </Row>
