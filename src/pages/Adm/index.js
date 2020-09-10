c569e135378efe05106c6bcb867576e039372c8c
Lucas Milhorato <lucaspradom14@gmail.com>
2020-09-10 10:48:00 -0300

    Gráfico funcionando!
    Mudanças no gráfico pra ele funcionar

--- src/pages/Adm/index.js
+++ src/pages/Adm/index.js
@@ -9,8 +9,12 @@ export default function Admin(props) {
     return (
         <div className="h-100">
             <Header onClick={() => setOpen(!open)} />
+            <div className = "h-100 d-flex">
             <SideBar open={open} />
+            <div className = "d-flex flex-column h-100 flex-grow-1">
             {props.children}
+            </div>
+            </div>
         </div>
     )
 }
