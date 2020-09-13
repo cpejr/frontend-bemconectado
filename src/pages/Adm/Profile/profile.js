dd3ee266c456b26b306cec6cb94189ff8e79391c
João Prates <jvdprates@hotmail.com>
2020-09-12 21:14:39 -0300

    Corrigindo 3

--- src/pages/Adm/Profile/profile.js
+++ src/pages/Adm/Profile/profile.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext, useEffect} from 'react';
-import InputEditable from '../../../components/InputEditabl'
+import InputEditable from '../../../components/InputEditable'
 import './styles.css';
 
 import { LoginContext } from '../../../contexts/LoginContext';
