3acf951ba374c85385281809d973499530a067c9
Diogo Almazan <diogodpa@outlook.com>
2020-09-17 13:18:42 -0300

    Adição do botão de salvar dados no Profile.

--- src/components/UserDataCard/index.js
+++ src/components/UserDataCard/index.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import { Save } from '@material-ui/icons';
 import InputEditable from "./InputEditable";
 import "./styles.css";
+
+import { LoginContext } from '../../contexts/LoginContext';
+import api from "../../services/api";
 
 export default function UserDataCard({
     mainTitle,
@@ -12,6 +15,36 @@ export default function UserDataCard({
     previousData,
     setData,
 }) {
+    
+    const { token } = useContext(LoginContext);
+
+    // const response = await api.get('/verify', config)
+    
+
+    async function handleSaveData(){
+
+        let body = new FormData();
+
+        function addToData(key, value) {
+            if (value !== undefined && value !== ''){
+                body.append(key, value)
+            }
+        }
+
+        genVector.forEach(item => {
+            addToData(item.objKey, data[item.objKey]);
+        })
+
+        alert('DEU CERTO?')
+        const config = {
+            headers: {
+                authorization: `Bearer ${token}`
+            },
+        }
+        console.log(data._id);
+        const response = await api.put(`/ong/${data._id}`, body, config);
+        
+    }
 
     return (
         <div className="cardInfo">
@@ -19,7 +52,9 @@ export default function UserDataCard({
             <div className="userProfile">
                 <div className="titleRow">
                     <h2>{mainTitle}</h2>
-                    <Save/>
+                    <div className="saveButton">
+                        <Save className="save" onClick={(e) => handleSaveData()}/>
+                    </div>
                 </div>
                 <p>{description}</p>
             </div>
