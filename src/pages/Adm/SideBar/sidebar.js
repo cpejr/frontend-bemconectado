2ae0b35102acbaeb043992fc4cbf8a7e7adb33e8
Lucas Milhorato <lucaspradom14@gmail.com>
2020-10-01 12:41:42 -0300

    Gráfico responsivo e atualização de icones

--- src/pages/Adm/SideBar/sidebar.js
+++ src/pages/Adm/SideBar/sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import ChevronRightIcon from '@material-ui/icons/ChevronRightIcon';
 // import MailIcon from '@material-ui/icons';
 // import ListItemIcon from '@material-ui/icons/Menu';
@@ -7,107 +7,136 @@ import React, { useEffect, useState } from 'react';
 
 // import AppBar from '@material-ui/core/AppBar';
 // import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
+import List from "@material-ui/core/List";
 // import CssBaseline from '@material-ui/core/CssBaseline';
 // import Typography from '@material-ui/core/Typography';
 // import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
+import IconButton from "@material-ui/core/IconButton";
 // import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import TodayIcon from "@material-ui/icons/Today";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import { useHistory } from "react-router-dom";
 
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-
-import './styles.css';
+import "./styles.css";
 
 const useStyles = makeStyles((theme) => ({
-        
-        drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        },
-        drawerClose: {
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            overflowX: 'hidden',
-            width: theme.spacing(7) + 1,
-            [theme.breakpoints.up('sm')]: {
-                width: theme.spacing(9) + 1,
-            },
-        },
-        drawer: {
-            height: "100%",
-            zIndex: 0
-        },
-        toolbar: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            padding: theme.spacing(0, 1),
-            // necessary for content to be below app bar
-            ...theme.mixins.toolbar,
-        }
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  drawer: {
+    height: "100%",
+    zIndex: 0,
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
 }));
 
 const drawerWidth = 240;
 
-export default function Header(props){
+export default function Header(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-    
-    // const handleDrawerOpen = () => {
-    //     setOpen(true);
-    // };
-    
-    const handleDrawerClose = () => {
-        setOpen(!open);
-    };
+  // const handleDrawerOpen = () => {
+  //     setOpen(true);
+  // };
 
-    useEffect(()=>{
-        if(typeof props.open !== "undefined"){
-            setOpen(props.open)
-        }
+  const handleDrawerClose = () => {
+    setOpen(!open);
+  };
 
-    }, [props]);
+  useEffect(() => {
+    if (typeof props.open !== "undefined") {
+      setOpen(props.open);
+    }
+  }, [props]);
+  const history = useHistory();
+  function navigate (page){
+    history.push(`/adm/${page}`)
+  }
+  return (
+    <Drawer
+      variant="permanent"
+      className={clsx(classes.drawer, {
+        [classes.drawerOpen]: open,
+        [classes.drawerClose]: !open,
+      })}
+      classes={{
+        paper: clsx({
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        }),
+      }}
+    >
+      <List>
+        <ListItem onClick = {()=>navigate("profile")}>
+          <ListItemIcon>
+            <AccountCircleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Perfil" />
+        </ListItem>
 
-    return (
-            <Drawer
+        <ListItem onClick = {()=>navigate("stats")}>
+          <ListItemIcon>
+            <AssessmentIcon />
+          </ListItemIcon>
+          <ListItemText primary="Análises" />
+        </ListItem>
 
-                variant="permanent"
-                className={clsx(classes.drawer, {
-                [classes.drawerOpen]: open,
-                [classes.drawerClose]: !open,
-                })}
-                classes={{
-                paper: clsx({
-                    [classes.drawerOpen]: open,
-                    [classes.drawerClose]: !open,
-                }),
-                }}
-            >
-                <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-                </List>
-            </Drawer>
+        <ListItem onClick = {()=>navigate("profile")}>
+          <ListItemIcon>
+            <MonetizationOnIcon />
+          </ListItemIcon>
+          <ListItemText primary="Financeiro" />
+        </ListItem>
 
-    )
+        <ListItem onClick = {()=>navigate("profile")}>
+          <ListItemIcon>
+            <TodayIcon />
+          </ListItemIcon>
+          <ListItemText primary="Campanhas" />
+        </ListItem>
+
+        <ListItem onClick = {()=>navigate("profile")}>
+          <ListItemIcon>
+            <ArrowBackIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sair" />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
 }
