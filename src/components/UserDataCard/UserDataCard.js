d78b4c8735be27ed4832451a360fe79ed48b94a6
João Prates <jvdprates@hotmail.com>
2020-10-08 10:48:34 -0300

    Adicionando catch de erro na página de editar usuário

--- src/components/UserDataCard/UserDataCard.js
+++ src/components/UserDataCard/UserDataCard.js
@@ -25,8 +25,8 @@ export default function UserDataCard({
 
     const { addToast } = useToasts();
 
-    useEffect(()=>{
-    },[update])
+    useEffect(() => {
+    }, [update])
 
     // function CustomToast(){
     //     return (
@@ -42,36 +42,42 @@ export default function UserDataCard({
 
         let body = {};
 
-        function addToData(key, value) {
-            if (value !== undefined && value !== '') {
-                body[key] = value;
+        try {
+            function addToData(key, value) {
+                if (value !== undefined && value !== '') {
+                    body[key] = value;
+                }
             }
+
+            genVector.forEach(item => {
+                addToData(item.objKey, data[item.objKey]);
+            })
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    authorization: `Bearer ${token}`
+                },
+            }
+
+            const response = await api.put(`/ong/${data._id}`, body, config);
+
+            const { accessToken, user } = response.data;
+
+            if (accessToken !== undefined && user !== undefined) {
+                logOut();
+                signIn(accessToken, user);
+                setData({ ...user });
+                setUpdate(!update);
+            }
+
+            setLoading(false);
+            addToast('Perfil atualizou com sucesso!', { appearance: 'success' });
+        } catch (err) {
+            console.log(err)
+            addToast('Falha em atualizar o perfil!', { appearance: 'error' });
+            setLoading(false);
         }
-
-        genVector.forEach(item => {
-            addToData(item.objKey, data[item.objKey]);
-        })
-
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-                authorization: `Bearer ${token}`
-            },
-        }
-
-        const response = await api.put(`/ong/${data._id}`, body, config);
-
-        const { accessToken, user } = response.data;
-
-        if (accessToken !== undefined && user !== undefined) {
-            logOut();
-            signIn(accessToken, user);
-            setData({...user});
-            setUpdate(!update);
-        }
-
-        setLoading(false);
-        addToast('Perfil atualizou com sucesso!', { appearance: 'success' });
     }
 
 
@@ -82,7 +88,7 @@ export default function UserDataCard({
                 <div className="titleRow">
                     <h2>{mainTitle}</h2>
                     <div className="saveButton">
-                        {loading ? 
+                        {loading ?
                             <ClipLoader size={30} color={"#000"} loading={true} />
                             : <Save className="save" onClick={(e) => handleSaveData()} />
                         }
